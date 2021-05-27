071be931d0cb1bf1f9c3515319ee11692c169996
Noah Day <sheltielover432@gmail.com>
2021-05-26 22:42:26 -0400

    #3 Player Shooting

--- GameDevTV_GameJam/Assets/Scripts/PlayerController.cs
+++ GameDevTV_GameJam/Assets/Scripts/PlayerController.cs
@@ -8,40 +8,65 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-    private Rigidbody2D rb;
+    [SerializeField] Transform firePoint;
+    [SerializeField] private GameObject cannonBall;
+    [SerializeField] private float projectileForce = 20f;
+    [SerializeField] private GameObject cannon = null;
+    private Rigidbody2D playerRb, cnRB;
     private SpriteRenderer sr;
-    private Vector2 movement;
-
+    private Vector2 movement, mousePos;
+    private Camera camera;
+    
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        playerRb = GetComponent<Rigidbody2D>();
+        cnRB = cannon.GetComponent<Rigidbody2D>();
         sr = GetComponent <SpriteRenderer>();
+        camera = FindObjectOfType<Camera>();//GetComponent<Camera>();
     }
 
     void Update()
     {
-        float tempX = Input.GetAxisRaw("Horizontal");
-        float tempY = Input.GetAxisRaw("Vertical");
-        
-        /*Debug.Log(tempX +", "+ tempY);
-        if (Mathf.Abs(tempX) > 0)
-        {*/
-            movement.x = tempX;
-        //}
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        /*if (Mathf.Abs(tempY) > 0)
-        {*/
-            movement.y = tempY;
-        //}
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        FixedPlayerMovement();
+       // FixedCannonMovement();
+    }
+
+    private void FixedCannonMovement()
+    {
+       
+        Vector2 lookDir = mousePos - cnRB.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        cnRB.rotation = angle;
+    }
+
+    private void FixedPlayerMovement()
+    {
+        playerRb.MovePosition(playerRb.position + movement * moveSpeed * Time.deltaTime);
         float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
         if (movement.x != 0 || movement.y != 0)
         {
+            //cnRB.position = new Vector2(playerRb.position.x + cannon.gameObject.transform.position.x,playerRb.position.y + cannon.gameObject.transform.position.y);
             transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         }
+    }
+    
+    private void Fire()
+    {
+        GameObject projectile = Instantiate(cannonBall, firePoint.position, firePoint.rotation);
+        Rigidbody2D rbProj = projectile.GetComponent<Rigidbody2D>();
+        rbProj.AddForce(mousePos * projectileForce, ForceMode2D.Impulse);
     }
 }
---
+++ GameDevTV_GameJam/Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void Awake()
+    {
+        Destroy(this, 5f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(this);
+    }
+}
