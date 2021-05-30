2fcd1428c0cd6b7d0c2b140cd871bc81acdfcde8
Noah Day <sheltielover432@gmail.com>
2021-05-30 15:41:15 -0400

    #6 Storm Borders

--- GameDevTV_GameJam/Assets/Scripts/GenerateEnvironment.cs
+++ GameDevTV_GameJam/Assets/Scripts/GenerateEnvironment.cs
@@ -8,7 +8,8 @@ public class GenerateEnvironment : MonoBehaviour
     [SerializeField] public int floorX = 20;
     [SerializeField] public int floorY = 20;
     [SerializeField] private GameObject waterTile;
-    
+    [SerializeField] private GameObject borderPrefab;
+    [SerializeField] private Sprite[] borderSprites;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,9 @@ public class GenerateEnvironment : MonoBehaviour
         RandomizeMap();
         GenerateFloor();
         MovePlayer();
+        GenerateBorder();
     }
+    
 
     // Update is called once per frame
     void Update()
@@ -26,8 +29,8 @@ public class GenerateEnvironment : MonoBehaviour
     
     private void RandomizeMap()
     {
-        floorX = Random.Range(20, 40);
-        floorY = Random.Range(20, 40);
+        floorX = Random.Range(40, 80);
+        floorY = Random.Range(40, 80);
     }
 
     private void GenerateFloor()
@@ -41,11 +44,50 @@ public class GenerateEnvironment : MonoBehaviour
         }
     }
 
+    private void GenerateBorder()
+    {
+        for (int x = -5; x < floorX +5; x++)
+        {
+            //Bottom Wall
+            for (int j = -7; j < 0; j++)
+            {
+                InstantiateWallTile(x, j);
+            }
+            //Top Wall
+            for (int j = 0; j < 5; j++)
+            {
+                InstantiateWallTile(x, floorY + j);
+            }
+        }
+
+        for (int y = -5; y < floorY + 5; y++)
+        {
+            //Left Wall
+            for (int j = -7; j < 0; j++)
+            {
+                InstantiateWallTile(j, y);
+            }
+
+            //Right Wall
+            for (int j = 0; j < 7; j++)
+            {
+                InstantiateWallTile(floorX + j, y); 
+            }
+        }
+    }
     private void InstantiateWaterTile(int x, int y)
     {
         GameObject newFloorTile = Instantiate(waterTile, new Vector2(x, y), Quaternion.identity); 
         newFloorTile.GetComponent<SpriteRenderer>().sortingOrder = 0;
         newFloorTile.transform.parent = this.transform;
+    }
+    
+    private void InstantiateWallTile(int x, int y)
+    {
+        GameObject newBorderTile = Instantiate(borderPrefab, new Vector2(x, y), Quaternion.identity);
+        newBorderTile.GetComponent<SpriteRenderer>().sprite = borderSprites[Random.Range(0, borderSprites.Length)];
+        newBorderTile.GetComponent<SpriteRenderer>().sortingOrder = 2;
+        newBorderTile.transform.parent = this.transform;
     }
     
     private void MovePlayer()
