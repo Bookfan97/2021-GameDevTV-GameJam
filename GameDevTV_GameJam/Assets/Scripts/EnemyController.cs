46b9c39b8a4f9999929e86da3345c2f52b7124d4
Noah Day <sheltielover432@gmail.com>
2021-05-29 23:54:04 -0400

    #2 Firing and Patroling

--- GameDevTV_GameJam/Assets/Scripts/EnemyController.cs
+++ GameDevTV_GameJam/Assets/Scripts/EnemyController.cs
@@ -4,59 +4,106 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private GameObject cannonBall;
+    [SerializeField] private float projectileForce = 20f;
+    [SerializeField] Transform firePoint;
     private PlayerController player;
-    private bool aggro = false;
-    private Vector3 targetPosition;
-    private float attackCounter;
-    public float waitAfterAttack = 3, minDistance;
+    public bool aggro = false, canFire = false;
+    public Vector3 targetPosition;
+    public float attackCounter, moveCounter;
+    public float waitAfterAttack = 3, waitToMove = 3, minDistance = 2, maxDistance = 5;
     public int batAttackSpeed = 5;
+
+    public float patrolTest = 0;
     // Start is called before the first frame update
     void Start()
     {
-        player = FindObjectOfType<PlayerController>();//GetComponent<PlayerController>().gameObject;
-        aggro = true;
+        player = FindObjectOfType<PlayerController>();
+        targetPosition = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (attackCounter > 0)
+        CheckDistanceToPlayer();
+        
+        if (moveCounter > 0)
         {
-            attackCounter -= Time.deltaTime;
-        }
-        if (aggro)
-        {
-            AttackPlayer();
+            moveCounter -= Time.deltaTime;
         }
         else
         {
-            CheckDistanceToPlayer();
+            MoveToPosition();
         }
+
+        if (aggro)
+        {
+            if (attackCounter > 0)
+            {
+                attackCounter -= Time.deltaTime;
+            }
+            else
+            {
+                AttackPlayer();
+            }
+        }
+        else
+        {
+            CheckForNewPosition();
+        }
+        
     }
 
     private void CheckDistanceToPlayer()
     {
-        float distance = 0;
-        if (distance <= minDistance)
+        if (Vector3.Distance(transform.position, player.transform.position) <= maxDistance)
         {
             aggro = true;
         }
-    }
+        else
+        {
+            aggro = false;
+            attackCounter = waitAfterAttack;
+        }
+    } 
 
     private void AttackPlayer()
     {
         if (attackCounter <= 0)
         {
-            if (targetPosition == Vector3.zero)
+            Fire();
+        }
+    }
+
+    private void MoveToPosition()
+    {
+        //CheckForNewPosition();
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, batAttackSpeed * Time.deltaTime);
+    }
+
+    void CheckForNewPosition()
+    {
+        if (Vector3.Distance(transform.position, targetPosition) <= .1f || targetPosition == Vector3.zero)
+        {
+            moveCounter = waitToMove;
+            if (aggro)
             {
-                targetPosition = player.gameObject.transform.position;
+                targetPosition = player.gameObject.transform.position + new Vector3(minDistance,minDistance,0);
             }
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, batAttackSpeed * Time.deltaTime);
-            if (Vector3.Distance(transform.position, targetPosition) <= .1f)
+            else if(!aggro)
             {
-                attackCounter = waitAfterAttack;
-                targetPosition = Vector3.zero;
+                patrolTest++;
+                targetPosition = new Vector3(Random.Range(-10,10),Random.Range(-10,10),0);
             }
         }
+    }
+    
+    private void Fire()
+    {
+        Debug.Log("FIRE");
+        GameObject projectile = Instantiate(cannonBall, firePoint.position, firePoint.rotation);
+        Rigidbody2D rbProj = projectile.GetComponent<Rigidbody2D>();
+        rbProj.AddForce(player.gameObject.transform.position * projectileForce, ForceMode2D.Impulse);
+        attackCounter = waitAfterAttack;
     }
 }
